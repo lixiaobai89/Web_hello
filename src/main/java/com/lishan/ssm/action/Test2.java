8a1639197677acd252a880152697791c1cb7d7af
lixiaobai89 <49465665+lixiaobai89@users.noreply.github.com>
2020-03-12 14:50:26 +0800

    提交测试 1 提交测试 2

--- src/main/java/com/lishan/ssm/action/Test2.java
+++ src/main/java/com/lishan/ssm/action/Test2.java
@@ -3,7 +3,7 @@ package com.lishan.ssm.action;
 public class Test2 {
     public static void main(String[] args) {
 
-        String s = null;//提交测试1
+        String s = null;//提交测试1 提交测试2
         System.out.println(s + "sss");
 
 
